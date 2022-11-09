693399f195c7d799881c9d586594613ba9d48cff
Ayse Sen <aysesen110211@gmail.com>
2022-11-10 00:58:04 +0300

    10.11

--- src/task11.java
+++ src/task11.java
@@ -14,6 +14,24 @@ public class task11 {
      */
 
     public static void main(String[] args) {
-        System.out.println("ayse branch selamlar");
+      int n=20;
+      int sayac=0;
+        for(int sayi=2;sayi<n;sayi++)
+        {
+            int kontrol = 0;
+            for (int i = 2; i < sayi; i++)
+            {
+                if (sayi % i == 0) {
+                    kontrol = 1;
+                    break;
+                }
+            }
+            if(kontrol==0)
+            {
+                System.out.print(sayi+" ");
+                sayac++;
+            }
+        }
+
     }
 }
